aceebfe8a3c122eee1817bd9ba7bd17a34fb8a2e
wenderf5 <wenderfabianoalves@gmail.com>
2023-11-11 17:49:13 -0300

    1.4

--- Js/Script.js
+++ Js/Script.js
@@ -12,103 +12,103 @@ var ht = document.getElementById("3h");
 var htr = ht.dataset.aberto;
 var hz = document.getElementById("4h");
 var hzr = hz.dataset.aberto;
-var hj = document.getElementById ("5h");
+var hj = document.getElementById("5h");
 var hjr = hj.dataset.aberto;
 var hk = document.getElementById("6h");
 var hkr = hk.textContent;
 var jk = document.getElementById("7h");
 var jkr = jk.dataset.aberto;
-function ax1(){
+function ax1() {
     if (oner == "1" &&
         twor == "2" &&
-        threer == "3"){
+        threer == "3") {
         oner = "3"
         twor = "1"
         threer = "2"
     } else if (
-            oner == "3" && 
-            twor == "1" &&
-            threer == "2"){
-                oner = "2"
-                twor = "3"
-                threer = "1"
-            } else if (
-                oner == "2" &&
-                twor == "3" &&
-                threer == "1"){
-                    oner = "1"
-                    twor = "2"
-                    threer = "3"
-                }
-    one.dataset.aberto = oner;
-    two.dataset.aberto = twor;
-    three.dataset.aberto = threer;
-}
-function ax2(){
-    if (oner == "1" &&
-        twor == "2" &&
-        threer == "3"){
+        oner == "3" &&
+        twor == "1" &&
+        threer == "2") {
         oner = "2"
         twor = "3"
         threer = "1"
     } else if (
-            oner == "2" && 
-            twor == "3" &&
-            threer == "1"){
-                oner = "3"
-                twor = "1"
-                threer = "2"
-            } else if (
-                oner == "3" &&
-                twor == "1" &&
-                threer == "2"){
-                    oner = "1"
-                    twor = "2"
-                    threer = "3"
-                }
+        oner == "2" &&
+        twor == "3" &&
+        threer == "1") {
+        oner = "1"
+        twor = "2"
+        threer = "3"
+    }
     one.dataset.aberto = oner;
     two.dataset.aberto = twor;
     three.dataset.aberto = threer;
 }
-function ax3(){
+function ax2() {
+    if (oner == "1" &&
+        twor == "2" &&
+        threer == "3") {
+        oner = "2"
+        twor = "3"
+        threer = "1"
+    } else if (
+        oner == "2" &&
+        twor == "3" &&
+        threer == "1") {
+        oner = "3"
+        twor = "1"
+        threer = "2"
+    } else if (
+        oner == "3" &&
+        twor == "1" &&
+        threer == "2") {
+        oner = "1"
+        twor = "2"
+        threer = "3"
+    }
+    one.dataset.aberto = oner;
+    two.dataset.aberto = twor;
+    three.dataset.aberto = threer;
+}
+function ax3() {
     if (har == "close" &&
         hbr == "close" &&
         htr == "close" &&
         hzr == "close" &&
         hjr == "close" &&
-        hkr == "Ver mais" ){
-            har = "open"
-            hbr = "open"
-            htr = "open"
-            hzr = "open"
-            hjr = "open"
-            hkr = "Ver menos"
-        } else if 
-       (har == "open" &&
+        hkr == "Ver mais") {
+        har = "open"
+        hbr = "open"
+        htr = "open"
+        hzr = "open"
+        hjr = "open"
+        hkr = "Ver menos"
+    } else if
+        (har == "open" &&
         hbr == "open" &&
         htr == "open" &&
         hzr == "open" &&
         hjr == "open" &&
         hkr == "Ver menos") {
-            har = "close"
-            hbr = "close"
-            htr = "close"
-            hzr = "close"
-            hjr = "close"
-            hkr = "Ver mais"
-        }
-        ha.dataset.aberto = har;
-        hb.dataset.aberto = hbr;
-        ht.dataset.aberto = htr;
-        hz.dataset.aberto = hzr;
-        hj.dataset.aberto = hjr;
-        hk.textContent = hkr;
-        }
-        function ax4(){
-            if (jkr == "close"){
-                jkr = "open"
-            }else if (jkr == "open"){
-                jkr = "close"
-            }
-            jk.dataset.aberto = jkr;
-        }
+        har = "close"
+        hbr = "close"
+        htr = "close"
+        hzr = "close"
+        hjr = "close"
+        hkr = "Ver mais"
+    }
+    ha.dataset.aberto = har;
+    hb.dataset.aberto = hbr;
+    ht.dataset.aberto = htr;
+    hz.dataset.aberto = hzr;
+    hj.dataset.aberto = hjr;
+    hk.textContent = hkr;
+}
+function ax4() {
+    if (jkr == "close") {
+        jkr = "open"
+    } else if (jkr == "open") {
+        jkr = "close"
+    }
+    jk.dataset.aberto = jkr;
+}
